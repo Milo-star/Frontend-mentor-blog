a5328a10103caec1d3c7b201292a856724303adf
Milo Bonnet <milovtt@gmail.com>
2023-02-24 09:54:11 +0100

    Add Javascript

---
+++ assets/js/survol.js
@@ -0,0 +1,36 @@
+// Github icon
+function survolgithub() {
+    document.getElementById("github").src = "assets/img/icon-github-active.svg";
+}
+
+function finSurvolgithub() {
+    document.getElementById("github").src = "assets/img/icon-github.svg";
+}
+
+// Frontend Mentor 
+function survolfront() {
+    document.getElementById("frontend").src = "assets/img/icon-frontend-mentor-active.svg";
+}
+
+function finSurvolfront() {
+    document.getElementById("frontend").src = "assets/img/icon-frontend-mentor.svg";
+}
+
+// Linkedin
+function survollinkedin() {
+    document.getElementById("linkedin").src = "assets/img/icon-linkedin-active.svg";
+}
+
+function finSurvollinkedin() {
+    document.getElementById("linkedin").src = "assets/img/icon-linkedin.svg";
+}
+
+// Twitter
+function survoltwitter() {
+    document.getElementById("twitter").src = "assets/img/icon-twitter-active.svg";
+}
+
+function finSurvoltwitter() {  
+    document.getElementById("twitter").src = "assets/img/icon-twitter.svg";
+}
+
