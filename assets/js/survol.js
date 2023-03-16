f62b75de3406256b463986ed7089de14bc5ab5d0
Milo Bonnet <milovtt@gmail.com>
2023-03-16 16:54:16 +0100

    del frontendmentor

--- assets/js/survol.js
+++ assets/js/survol.js
@@ -7,15 +7,6 @@ function finSurvolgithub() {
     document.getElementById("github").src = "assets/img/icon-github.svg";
 }
 
-// Frontend Mentor 
-function survolfront() {
-    document.getElementById("frontend").src = "assets/img/icon-frontend-mentor-active.svg";
-}
-
-function finSurvolfront() {
-    document.getElementById("frontend").src = "assets/img/icon-frontend-mentor.svg";
-}
-
 // Linkedin
 function survollinkedin() {
     document.getElementById("linkedin").src = "assets/img/icon-linkedin-active.svg";
